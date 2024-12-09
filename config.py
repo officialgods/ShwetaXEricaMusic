41a4c795399b0d5e1c33023cc2e778fbecbc2784
Rani0086 <shantidhaakad@gmail.com>
2024-12-09 17:42:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,11 +15,11 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002298711889))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",-1002401739175))
 
-OWNER_ID = int(getenv("OWNER_ID", 5886080930))
+OWNER_ID = int(getenv("OWNER_ID", 7825870013))
 
-OWNER = int(getenv("OWNER", 5886080930))
+OWNER = int(getenv("OWNER", 7825870013))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
@@ -33,8 +33,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TENJIKU_BOT_SUPPORTER")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/TENJIKU_ARA")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_Revengers_Network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/anime_x_aegis")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "2a230af10e0a40638dc77c1febb47170")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "7f92897a59464ddbbf00f06cd6bda7fc")
@@ -57,10 +57,10 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/edu8v2.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/edu8v2.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/z7meid.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/z7meid.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/edu8v2.jpg"
-STATS_IMG_URL = getenv("STATS_IMG_URL", "https://files.catbox.moe/edu8v2.jpg")
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://files.catbox.moe/z7meid.jpg")
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/edu8v2.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/edu8v2.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/edu8v2.jpg"
